81b53e85d44d824494eebac39ef5fc09a808c77b
Simphiwe <sthsim007@myuct.ac.za>
2021-05-20 15:58:14 +0200

    first push

---
+++ main.py
@@ -0,0 +1,21 @@
+#Basic calculator
+
+class Add:
+    def __init__(self):
+        pass
+
+    def add(self, a, b):
+        return a + b
+
+class Subtract:
+    def __init__(self):
+        pass
+
+
+    def subtract(self):
+Class Divide:
+    def __init__ (self):
+        pass
+
+class Multiply(self)
+
